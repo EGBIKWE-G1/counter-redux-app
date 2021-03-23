84c1ccdb3ef3c0eda4a4f78b9c339ba6eae7cea0
unknown <godwinprofessional@gmail.com>
2021-03-23 15:09:56 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
+
+// export const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+
+// export const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
